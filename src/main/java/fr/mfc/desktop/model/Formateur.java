89eed67d37d21d1e353043184a92de18f166b7fb
Abdessamad EL OUASSAI <elouassai.a@gmail.com>
2026-06-18 02:17:33 +0200

    Initial commit - application-mfc

---
+++ src/main/java/fr/mfc/desktop/model/Formateur.java
@@ -0,0 +1,59 @@
+package fr.mfc.desktop.model;
+
+public class Formateur {
+    private String matricule;
+    private String nom;
+    private String prenom;
+    private String email;
+    private String tel;
+    private String specialite;
+
+    public String getMatricule() {
+        return matricule;
+    }
+
+    public void setMatricule(String matricule) {
+        this.matricule = matricule;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getSpecialite() {
+        return specialite;
+    }
+
+    public void setSpecialite(String specialite) {
+        this.specialite = specialite;
+    }
+}
+
